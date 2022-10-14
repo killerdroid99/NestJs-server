a5eeef6aca096a9a1c962665911d4cc2428a7629
killerdroid99 <ajempire09@gmail.com>
2022-10-14 14:03:54 +0530

    User module and service removal

--- src/users/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { UsersService } from './users.service';
-
-@Module({
-  exports: [UsersService],
-  providers: [UsersService, PrismaService],
-})
-export class UsersModule {}

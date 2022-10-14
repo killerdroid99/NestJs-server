8cd7c8a3704e73bd1faa671c4ccbb056f4399942
killerdroid99 <ajempire09@gmail.com>
2022-10-14 14:04:58 +0530

    providers for local strategy auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
-import { UsersModule } from 'src/users/users.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
+import { SessionSerializer } from './session.serializer';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, SessionSerializer, PrismaService],
 })
 export class AuthModule {}

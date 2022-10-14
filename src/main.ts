c9e30e1c255fe98393d90c5312a578e513066963
killerdroid99 <ajempire09@gmail.com>
2022-10-14 14:01:27 +0530

    cookie configuration and session store init

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import { PrismaClient } from '@prisma/client';
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+  const prisma = new PrismaClient();
+  app.use(
+    session({
+      name: 'qid',
+      secret: process.env.COOKIE_SECRET,
+      saveUninitialized: false,
+      resave: false,
+      cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days expiration time
+        // maxAge: 1000 * 30, // 30 seconds expiration time for testing purposes
+      },
+      store: new PrismaSessionStore(prisma, {
+        checkPeriod: 2 * 60 * 1000, //ms
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }),
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.enableCors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  });
+  await app.listen(process.env.PORT);
 }
 bootstrap();

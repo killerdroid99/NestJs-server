05277b143ede59e463da30d31a83c74c1f038393
killerdroid99 <ajempire09@gmail.com>
2022-10-14 14:00:57 +0530

    passport module setup for sessions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [PostsModule, AuthModule, UsersModule],
+  imports: [
+    PostsModule,
+    AuthModule,
+    PassportModule.register({
+      session: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })

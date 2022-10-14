439075a1a9bb3bfb89c1aa8f36c0e38b0dce7828
killerdroid99 <ajempire09@gmail.com>
2022-10-14 14:07:46 +0530

    setting up routes for login, logout, signup and other testing routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,18 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+  Session,
+  UseGuards,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
+import { Request, Response } from 'express';
 import { AuthService } from './auth.service';
-import { LocalAuthGuard } from './local-auth.guard';
+import { LocalAuthGuard, AuthenticatedGuard } from './local-auth.guard';
 
 export type CreateUserInput = {
   username: string;
@@ -8,20 +20,46 @@ export type CreateUserInput = {
   confirmPassword: string;
 };
 
+interface UserRequest {
+  user: User;
+}
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(@Request() req) {
-    return {
-      msg: `${req.user.username}, you are now successfully authenticated`,
-    };
+  @UseGuards(LocalAuthGuard)
+  async login(@Req() req: UserRequest, @Res() res: Response) {
+    return res.send(HttpStatus.OK);
+  }
+
+  @Post('logout')
+  @UseGuards(AuthenticatedGuard)
+  async logout(@Req() req: Request, @Res() res: Response) {
+    req.logout((err) => {
+      return err ? res.send(400) : res.send(200);
+    });
   }
 
   @Post('signup')
-  signUp(@Request() req, @Body() input: CreateUserInput) {
+  signUp(@Req() req, @Body() input: CreateUserInput) {
     return this.authService.userSignup(input);
+  }
+
+  @Get('')
+  async getAuthSession(@Req() req, @Session() session: Record<string, any>) {
+    return session;
+  }
+
+  @Get('a')
+  @UseGuards(AuthenticatedGuard)
+  a() {
+    return { msg: 'you are authorized to see this message' };
+  }
+
+  @Get('status')
+  @UseGuards(AuthenticatedGuard)
+  async status(@Req() req: Request, @Res() res: Response) {
+    res.send(req.user);
   }
 }

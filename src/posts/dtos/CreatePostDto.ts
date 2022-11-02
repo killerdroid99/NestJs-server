6e1199b1afade54af403f3ef66f49d5337753089
killerdroid99 <ajempire09@gmail.com>
2022-11-02 12:34:10 +0530

    setting max length on post title

--- src/posts/dtos/CreatePostDto.ts
+++ src/posts/dtos/CreatePostDto.ts
@@ -1,7 +1,8 @@
-import { MinLength } from 'class-validator';
+import { MaxLength, MinLength } from 'class-validator';
 
 export class CreatePostDto {
   @MinLength(5)
+  @MaxLength(50)
   title: string;
 
   content?: string;
